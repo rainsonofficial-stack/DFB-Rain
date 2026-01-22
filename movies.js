26d4fc42ff33ef6d63818fa63389c77e71da5ddb
rainsonofficial-stack <rainsonofficial@gmail.com>
2026-01-22 21:16:56 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,12 @@
+const movieData = {
+  title: "CINEMA",
+  forceWords: ["Inception", "The Matrix"],
+  items: [
+    "Jaws", "Star Wars", "Avatar", "Psycho", "Vertigo", "Rocky", "Alien", "Platoon", "Frozen", "Up",
+    "Heat", "Speed", "Scream", "Signs", "Saw", "Clue", "Seven", "Brave", "Coco", "Cars",
+    "Dune", "Thor", "Hulk", "Gump", "Lion", "Amity", "Fargo", "Mulan", "Shrek", "Glory",
+    "Babe", "Rush", "Milk", "Joker", "Logan", "Bambi", "Dumbo", "Shaun", "Ted", "Signs",
+    "Seven", "Glass", "Split", "Pearl", "X", "Bolt", "Rio", "Sing", "Soul", "Zion"
+  ]
+};
+
