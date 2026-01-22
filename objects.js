998b539e9e91b8fb1a7cdcfea4ff6fd90ca219dd
rainsonofficial-stack <rainsonofficial@gmail.com>
2026-01-22 21:33:01 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,11 @@
+const objectData = {
+  title: "Objects",
+  forceWords: ["balloon", "wallet"],
+  items: [
+    "keys", "watch", "phone", "pen", "notebook", "bottle", "glasses", "hat", "shoe", "belt",
+    "ring", "coin", "charger", "cable", "mouse", "keyboard", "remote", "candle", "mirror", "pillow",
+    "blanket", "mug", "spoon", "fork", "plate", "backpack", "umbrella", "towel", "soap", "comb",
+    "brush", "scissors", "tape", "ruler", "calculator", "flashlight", "battery", "clock", "photo", "frame",
+    "book", "magazine", "plant", "vase", "speaker", "headphones", "tripod", "lens", "ticket", "passport"
+  ]
+};
