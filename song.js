e0a2fbcd5e16cbdb10e90ab0aaa1748eed953d5c
rainsonofficial-stack <rainsonofficial@gmail.com>
2026-01-23 12:54:22 +0530

    Create song.js

---
+++ song.js
@@ -0,0 +1,17 @@
+const songData = {
+    title: "Songs",
+    forceWords: ["Waka Waka", "Never Say Never"],
+    items: [
+        "Shake It Off", "Lose Yourself", "Sugar", "Yellow", "Dynamite",
+        "Baby", "What Makes You Beautiful", "Shape of You", "Cheri Cheri Lady", "Viva La Vida",
+        "Blank Space", "Without Me", "Moves Like Jagger", "Perfect", "Butter",
+        "Sorry", "Story of My Life", "Thinking Out Loud", "You're My Heart, You're My Soul", "Fix You",
+        "Love Story", "The Real Slim Shady", "Girls Like You", "Bad Habits", "Boy With Luv",
+        "Love Yourself", "Best Song Ever", "Castle on the Hill", "Brother Louie", "The Scientist",
+        "You Belong With Me", "Not Afraid", "She Will Be Loved", "A Sky Full of Stars", "Permission to Dance",
+        "What Do You Mean?", "Drag Me Down", "Geronimo's Cadillac", "Stay", "Uptown Funk",
+        "Anti-Hero", "Love The Way You Lie", "Memories", "Fake Love", "Steal My Girl",
+        "Atlantis Is Calling", "Roar", "Counting Stars", "Titanium", "Rolling in the Deep"
+    ]
+};
+
