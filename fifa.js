0cd74634a5607b24a5e74fcb08083c0708d9da36
rainsonofficial-stack <rainsonofficial@gmail.com>
2026-07-26 01:58:53 +0530

    Create fifa.js

---
+++ fifa.js
@@ -0,0 +1,14 @@
+const fifaData = {
+  title: "FIFA countries",
+  forceWords: ["Mexico", "USA"],
+  items: [
+    "Canada",
+    "Australia", "Iraq", "IR Iran", "Japan", "Jordan", "Korea Republic", "Qatar", "Saudi Arabia", "Uzbekistan",
+    "Algeria", "Cabo Verde", "Congo DR", "Côte d'Ivoire", "Egypt", "Ghana", "Morocco", "Senegal", "South Africa", "Tunisia",
+    "Curaçao", "Haiti", "Panama",
+    "Argentina", "Brazil", "Colombia", "Ecuador", "Paraguay", "Uruguay",
+    "New Zealand",
+    "Austria", "Belgium", "Bosnia and Herzegovina", "Croatia", "Czechia", "England", "France", "Germany", "Netherlands", "Norway", "Portugal", "Scotland", "Spain", "Sweden", "Switzerland", "Türkiye",
+    "Italy", "Nigeria"
+  ]
+};
