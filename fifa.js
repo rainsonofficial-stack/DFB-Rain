cc938159e40f80a172da81069e0bb53a98b10be6
rainsonofficial-stack <rainsonofficial@gmail.com>
2026-07-26 09:31:42 +0530

    Update fifa.js

--- fifa.js
+++ fifa.js
@@ -6,9 +6,9 @@ const fifaData = {
     "Australia", "Iraq", "IR Iran", "Japan", "Jordan", "Korea Republic", "Qatar", "Saudi Arabia", "Uzbekistan",
     "Algeria", "Cabo Verde", "Congo DR", "Côte d'Ivoire", "Egypt", "Ghana", "Morocco", "Senegal", "South Africa", "Tunisia",
     "Curaçao", "Haiti", "Panama",
-    "Argentina", "Brazil", "Colombia", "Ecuador", "Paraguay", "Uruguay",
+    "Argentina", "Nigeria", "Colombia", "Ecuador", "Paraguay", "Uruguay",
     "New Zealand",
     "Austria", "Belgium", "Bosnia and Herzegovina", "Croatia", "Czechia", "England", "France", "Germany", "Netherlands", "Norway", "Portugal", "Scotland", "Spain", "Sweden", "Switzerland", "Türkiye",
-    "Italy", "Nigeria"
+    "Italy", "Brazil"
   ]
 };
